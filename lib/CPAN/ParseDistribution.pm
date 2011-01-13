188c806c414ba5a46aa9da86597d6bc17ac2e574
David Cantrell <david@cantrell.org.uk>
2011-01-13 22:17:58 +0000

    Import of DCANTRELL/CPAN-ParseDistribution-1.3 from CPAN.
    gitpan-cpan-distribution: CPAN-ParseDistribution
    gitpan-cpan-version:      1.3
    gitpan-cpan-path:         DCANTRELL/CPAN-ParseDistribution-1.3.tar.gz
    gitpan-cpan-author:       DCANTRELL
    gitpan-cpan-maturity:     released

--- lib/CPAN/ParseDistribution.pm
+++ lib/CPAN/ParseDistribution.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use vars qw($VERSION);
 
-$VERSION = '1.2';
+$VERSION = '1.3';
 
 use Cwd qw(getcwd abs_path);
 use File::Temp qw(tempdir);
@@ -218,13 +218,14 @@ sub _parse_version_safely {
 
 Returns true or false depending on whether this is a developer-only
 or trial release of a distribution.  This is determined by looking for
-an underscore in the distribution version.
+an underscore in the distribution version or the string '-TRIAL' at the
+end of the distribution version.
 
 =cut
 
 sub isdevversion {
     my $self = shift;
-    return 1 if($self->distversion() =~ /_/);
+    return 1 if($self->distversion() =~ /(_|-TRIAL$)/);
     return 0;
 }
 
@@ -376,7 +377,7 @@ L<dumpcpandist>
 
 =head1 AUTHOR, COPYRIGHT and LICENCE
 
-Copyright 2009-2010 David Cantrell E<lt>david@cantrell.org.ukE<gt>
+Copyright 2009-2011 David Cantrell E<lt>david@cantrell.org.ukE<gt>
 
 Contains code originally from the PAUSE by Andreas Koenig.
 
